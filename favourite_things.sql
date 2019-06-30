2092a3ff21158f2293b44b44f9522448496a00f8
Rahat Hossain Samit <rahat106@yahoo.com>
2019-06-30 15:53:31 +0600

    schema added

--- favourite_things.sql
+++ favourite_things.sql
@@ -16,14 +16,46 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`favourite_things` /*!40100 DEFAULT CHAR
 
 USE `favourite_things`;
 
-/*Data for the table `category` */
+/*Table structure for table `category` */
 
-insert  into `category`(`id`,`name`,`created_date`,`modified_date`) values 
-(1,'food',NULL,NULL);
+DROP TABLE IF EXISTS `category`;
 
-/*Data for the table `item_metadata` */
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `modified_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-/*Data for the table `items` */
+/*Table structure for table `item_metadata` */
+
+DROP TABLE IF EXISTS `item_metadata`;
+
+CREATE TABLE `item_metadata` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `meta_type` enum('DATE','TEXT','NUMBER','ENUM') DEFAULT NULL,
+  `item_id` int(11) DEFAULT NULL,
+  `key` varchar(20) DEFAULT NULL,
+  `value` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `items` */
+
+DROP TABLE IF EXISTS `items`;
+
+CREATE TABLE `items` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `desc` text COLLATE utf8mb4_unicode_ci,
+  `ranking` int(11) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `item_metadata` json DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `modified_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
